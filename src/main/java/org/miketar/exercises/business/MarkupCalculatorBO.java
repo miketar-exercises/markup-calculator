88de38308a1150606cc006bfe5b31760c7bb050d
Mickael Tardy <mishgunn@gmail.com>
2016-12-18 19:48:58 -0500

    Introduce basic features in Markup Calculator interface

--- src/main/java/org/miketar/exercises/business/MarkupCalculatorBO.java
+++ src/main/java/org/miketar/exercises/business/MarkupCalculatorBO.java
@@ -1,5 +1,38 @@
 package org.miketar.exercises.business;
 
+/**
+ * Markup calculator is responsible of calculating the markup according to the
+ * specified rules as well as calculating of the final cost of the project
+ * 
+ * @author mickaeltardy
+ *
+ */
 public interface MarkupCalculatorBO {
+
+	/**
+	 * Calculate the markup based on the number of people involved in the job
+	 * and category of the product
+	 * 
+	 * @param personnelCnt
+	 *            number of people involved, integer
+	 * @param category
+	 *            category of the product, string
+	 * @return
+	 */
+	public double calculateMarkup(int peopleCnt, String category);
+
+	/**
+	 * Calculate the final cost of the project using the initial price and based
+	 * on the number of personal involved and category of the product
+	 * 
+	 * @param price
+	 *            initial base price, double
+	 * @param peopleCnt
+	 *            number of people involved, integer
+	 * @param category
+	 *            category of the product, string
+	 * @return
+	 */
+	public double calculateFinalCost(double basePrice, int peopleCnt, String category);
 
 }
--- src/main/java/org/miketar/exercises/business/MarkupCalculatorBOImpl.java
+++ src/main/java/org/miketar/exercises/business/MarkupCalculatorBOImpl.java
@@ -1,5 +1,21 @@
 package org.miketar.exercises.business;
 
+/**
+ * Basic implementation of {@link MarkupCalculatorBO}
+ * 
+ * @author mickaeltardy
+ *
+ */
 public class MarkupCalculatorBOImpl implements MarkupCalculatorBO {
+
+	public double calculateMarkup(int peopleCnt, String category) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double calculateFinalCost(double basePrice, int peopleCnt, String category) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
