bbd5c96dcfda7b76c6102c25f2f4c0328a4d2c96
Mickael Tardy <mishgunn@gmail.com>
2016-12-18 20:26:47 -0500

    Export Constants in separate file

---
+++ src/main/java/org/miketar/exercises/business/MarkupConstants.java
@@ -0,0 +1,32 @@
+package org.miketar.exercises.business;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MarkupConstants {
+
+	/**
+	 * Flat markup value
+	 */
+	protected final static double FLAT_MARKUP = 0.05;
+
+	/**
+	 * One Person markup value
+	 */
+	protected final static double PERSON_MARKUP = 0.012;
+
+	/**
+	 * Categories markups
+	 */
+	protected static final Map<String, Double> CATEGORIES_MARKUP;
+
+	static {
+		Map<String, Double> catsMarkup = new HashMap<String, Double>();
+		catsMarkup.put("food", 0.13);
+		catsMarkup.put("drugs", 0.075);
+		catsMarkup.put("electronics", 0.02);
+		CATEGORIES_MARKUP = Collections.unmodifiableMap(catsMarkup);
+	}
+
+}
